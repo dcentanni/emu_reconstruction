1a688f8e0763098baef6824379c4609734caa07a
dcentanni <daniele.centanni@outlook.it>
2024-07-30 16:05:03 +0200

    Changes according to dannc user

--- beam/condor_ab0.sh
+++ beam/condor_ab0.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
-RUN= #put run number
-BRICKID= #put brick id
+RUN=$1
+BRICKID=$2
 BRICKFOLDER="$(printf "b%0*d" 6 $BRICKID)"
 PLATENUMBER=$3
 
 echo "Set up SND environment"
-SNDBUILD_DIR= #put sndsw /sw path
+SNDBUILD_DIR=/afs/cern.ch/user/d/dannc/sw
 source /cvmfs/sndlhc.cern.ch/SNDLHC-2023/Aug30/setUp.sh
 eval `alienv load -w $SNDBUILD_DIR --no-refresh sndsw/latest`
-echo "Loading FEDRA"
-source #put /fedra/setup.sh 	
+source /afs/cern.ch/user/d/dannc/setup_fedrarelease.sh
 
 echo  "go into reconstruction folder "
-cd # put reconstruction fodler /RUN$RUN/$BRICKFOLDER
+cd /eos/experiment/sndlhc/users/dancc/emureco/Napoli/RUN${RUN}/${BRICKFOLDER}
 
 echo "mosalignbeam $BRICKID.$PLATENUMBER.0.0"
 source mosalignbeam.sh $BRICKID $PLATENUMBER
